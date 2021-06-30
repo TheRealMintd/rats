1642a1c404cbb5d94c4c33c884dd177ffa665fc1
Mintd <johnmax2468@gmail.com>
2021-06-30 18:42:49 +0800

    Add linters and formatters

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: "module",
+		tsconfigRootDir: __dirname,
+		project: ["./tsconfig.json"],
+	},
+	plugins: ["@typescript-eslint", "import"],
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-requiring-type-checking",
+		"plugin:import/recommended",
+		"plugin:import/typescript",
+		"prettier",
+	],
+	rules: {
+		"@typescript-eslint/naming-convention": "warn",
+		"@typescript-eslint/semi": "warn",
+		"import/no-unresolved": "off",
+		"import/order": ["warn", { "newlines-between": "always" }],
+		curly: "warn",
+		eqeqeq: "warn",
+		"no-throw-literal": "warn",
+		semi: "off",
+	},
+	ignorePatterns: ["out", "dist", "**/*.d.ts"],
+};
