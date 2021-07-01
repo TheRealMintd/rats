e1fa5a6c669d88a8b48e049d44485a40fd1b9df4
Mintd <johnmax2468@gmail.com>
2021-07-01 12:29:01 +0800

    Setup basic game data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import React from "react";
+import { Client } from "boardgame.io/react";
 
-import logo from "./logo.svg";
-import "./App.css";
+import { Rats } from "./Game";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-function App(): JSX.Element {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+const App = Client({ game: Rats });
 
 export default App;
--- src/Game.ts
+++ src/Game.ts
@@ -1,1 +1,22 @@
-export const Rats = {};
+import { Game } from "boardgame.io";
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/naming-convention */
+export const Rats: Game = {
+	maxPlayers: 6,
+	setup: (ctx) => ({
+		round: 0,
+		banquetGoals: Array(5),
+		playerData: new Array(ctx.numPlayers)
+			.fill(0)
+			.map(() => ({
+				cocktailSwords: 0,
+				baubles: 0,
+				straw: 0,
+				crumbs: 0,
+				rags: 0,
+				flowers: 0,
+				dishes: [],
+				decorations: [],
+			})),
+	}),
+};
