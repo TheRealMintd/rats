b2ea35a1687da60cd7d6ba0dfa0dce61ede8fb1a
yuhueilee <ylee0026@student.monash.edu>
2022-02-01 14:55:14 +0800

    Add scavenge board tsx file

---
+++ src/boards/ScavengeBoard.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react";
+
+import '../styles/board.css';
+
+const images = [
+    '/cocktailswords.png', 
+    '/baubles.png', 
+    '/straw.png', 
+    '/crumbs.png', 
+    '/rags.png', 
+    '/flowers.png'
+];
+
+export const ScavengeBoard = (): ReactElement => {
+    return (
+        <div>
+        </div>
+    );
+};
