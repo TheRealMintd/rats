2c0c23d9b7b9ce0a739fe7422dbc9760ace73b5b
yuhueilee <ylee0026@student.monash.edu>
2022-02-01 15:27:21 +0800

    Add wrapper to banquet images

--- src/boards/BanquetBoard.tsx
+++ src/boards/BanquetBoard.tsx
@@ -23,12 +23,14 @@ export const BanquetBoard = (): ReactElement => {
         <div className='row'>
             <div className='column left'>
                 <h1 className='heading'>Banquet Goal</h1>
-                <div>
+                <div className='banquet-wrapper'>
                     <img className='banquet-img' src={banquetImages[0]} id='cheap'/>
                     <img className='banquet-img' src={banquetImages[1]} id='composed'/>
                     <img className='banquet-img' src={banquetImages[2]} id='greedy'/>
                     <img className='banquet-img' src={banquetImages[3]} id='refined'/>
                     <img className='banquet-img' src={banquetImages[4]} id='generous'/>
+                </div>
+                <div className='banquet-wrapper'>
                     <img className='banquet-img' src={banquetImages[5]} id='plush'/>
                     <img className='banquet-img' src={banquetImages[6]} id='swanky'/>
                     <img className='banquet-img' src={banquetImages[7]} id='dapper'/>
