c76c19426929fc0eed6972103ca6e9b3e2d2e163
Wong Yi Xiong <johnmax2468@gmail.com>
2022-02-13 17:14:54 +0800

    Rewrite DiceBoard as a class to use Refs
    Also run through Prettier

--- src/boards/DiceBoard.tsx
+++ src/boards/DiceBoard.tsx
@@ -1,51 +1,74 @@
-import { ReactElement } from "react";
+import React from "react";
 
-import '../styles/board.css';
-import { banquetGoals } from './BanquetBoard';
+import "../styles/board.css";
+import { banquetGoals } from "./BanquetBoard";
 
-const images = [
-    '/dice-1.png', 
-    '/dice-2.png', 
-    '/dice-3.png', 
-    '/dice-4.png', 
-    '/dice-5.png', 
-    '/dice-6.png'
-];
+export class DiceBoard extends React.Component {
+	private diceOne: React.RefObject<HTMLImageElement>;
+	private diceTwo: React.RefObject<HTMLImageElement>;
+	private total: React.RefObject<HTMLParagraphElement>;
 
-export const roll = (): void => {
-    const dice = [
-        document.querySelector('#die-1'),
-        document.querySelector('#die-2'),
-    ];
-    dice.forEach(function(die) {
-        die?.classList.add('shake');
-    });
-    setTimeout(function() {
-        dice.forEach(function(die) {
-            die?.classList.remove('shake');
-        });
-        const dieOneValue: number = Math.floor(Math.random() * 6);
-        const dieTwoValue: number = Math.floor(Math.random() * 6);
-        const total: number = dieOneValue + 1 + dieTwoValue + 1;
-        document.querySelector('#die-1')?.setAttribute('src', images[dieOneValue]);
-        document.querySelector('#die-2')?.setAttribute('src', images[dieTwoValue]);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        document.querySelector('#total')!.innerHTML = 'The banquet goal is ' + total.toString();
-        document.querySelector(banquetGoals[total - 2])?.classList.add('shadow');
-    },
-    1000
-    );
-};
+	constructor(props: Record<string, never>) {
+		super(props);
+		this.diceOne = React.createRef();
+		this.diceTwo = React.createRef();
+		this.total = React.createRef();
+	}
 
-export const DiceBoard = (): ReactElement => {
-    return (
-        <div className='dice-container'>
-            <div className='dice-wrapper'>
-                <img className='dice-img' src='/dice-1.png' id='die-1'/>
-                <img className='dice-img' src='/dice-2.png' id='die-2'/>
-            </div>
-            <p className='msg' id='total'></p>
-            <button className='button-19' role='button' onClick={() => roll()}>Roll Banquet Goal</button>
-        </div>
-    );
-};
+	private roll(): void {
+		this.diceOne.current?.classList.add("shake");
+		this.diceTwo.current?.classList.add("shake");
+
+		setTimeout(() => {
+			this.diceOne.current?.classList.remove("shake");
+			this.diceTwo.current?.classList.remove("shake");
+
+			const dieOneValue = Math.floor(Math.random() * 6) + 1;
+			const dieTwoValue = Math.floor(Math.random() * 6) + 1;
+			this.diceOne.current?.setAttribute(
+				"src",
+				`/dice-${dieOneValue}.png`
+			);
+			this.diceTwo.current?.setAttribute(
+				"src",
+				`/dice-${dieTwoValue}.png`
+			);
+
+			const total = dieOneValue + dieTwoValue;
+			const paragraph = this.total.current;
+			if (paragraph) {
+				paragraph.textContent = `The banquet goal is ${total}`;
+			}
+			document
+				.querySelector(banquetGoals[total - 2])
+				?.classList.add("shadow");
+		}, 1000);
+	}
+
+	override render(): React.ReactNode {
+		return (
+			<div className="dice-container">
+				<div className="dice-wrapper">
+					<img
+						className="dice-img"
+						src="/dice-1.png"
+						ref={this.diceOne}
+					/>
+					<img
+						className="dice-img"
+						src="/dice-2.png"
+						ref={this.diceTwo}
+					/>
+				</div>
+				<p className="msg" ref={this.total}></p>
+				<button
+					className="button-19"
+					role="button"
+					onClick={() => this.roll()}
+				>
+					Roll Banquet Goal
+				</button>
+			</div>
+		);
+	}
+}
